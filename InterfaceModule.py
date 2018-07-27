303d0df7f0178888c7ba333c952a8c7c6dbb4e5e
帅 张 <1290645535@qq.com>
2018-07-27 21:02:56 +0800

    为get_sheet增加了提供表头信息dict的功能和自动缓存数据的功能 重写了get_sheet的API，使之可以提供表头+数据的sheet信息；使用pickle和cache装饰器使得get_sheet可以自动缓存被生成过的sheet信息 设计中可能的缺陷都写在了函数注释的"注意"中

--- InterfaceModule.py
+++ InterfaceModule.py
@@ -1,6 +1,26 @@
-# coding:utf-8
-from win32com.client import Dispatch
+# -*- coding: utf-8 -*-
 import win32com.client
+import os
+import pickle
+
+
+def cache(get_sheet):
+    """Easyexcel类中get_sheet函数的装饰器，用于自动缓存被使用过的sheet
+    注意1：若被处理的Excel表不再是’about/2018年04道普业务提成明细.xlsx’，这里需要修改
+    注意2：若sheet被修改了，将cached_sheets下对应的pickle文件删除即可，之后调用get_sheet时会自动生成"""
+    def inner(self, sheet_name):
+        if os.path.exists("cached_sheets/" + sheet_name + ".pickle"):
+            print("exists")
+            with open("cached_sheets/" + sheet_name + ".pickle", "rb") as f:
+                return pickle.load(f)
+        else:
+            print("gen")
+            header_dict, sheet_data = get_sheet(self, sheet_name)
+            with open("cached_sheets/" + sheet_name + ".pickle", "wb") as f:
+                pickle.dump((header_dict, sheet_data), f)
+            return header_dict, sheet_data
+
+    return inner
 
 
 class Easyexcel:
@@ -15,30 +35,49 @@ class Easyexcel:
             self.xlBook = self.xlApp.Workbooks.Add()
             self.filename = ''
 
+    def get_a_row(self, sheet_name, r, col_num=-1):
+        """col_num<0,根据末尾连续空格数决定此行是否终止(用于读取表头);col_num>=0,读入长度为col_num的一行(用于读取普通数据)
+        注意：在指定col_num情况下，该行只要有一个值不是None就会被返回整行，只有全为None时才会返回[]"""
+        row = []
+        if col_num < 0:
+            c = 1  # col，本行要读入的列号，从1开始计数
+            while str(self.xlBook.Worksheets(sheet_name).Cells(r, c)) != 'None' \
+                    or str(self.xlBook.Worksheets(sheet_name).Cells(r, c + 1)) != 'None' \
+                    or str(self.xlBook.Worksheets(sheet_name).Cells(r, c + 2)) != 'None':
+                row.append(str(self.xlBook.Worksheets(sheet_name).Cells(r, c)))
+                c += 1
+        else:
+            for c in range(1, col_num + 1):
+                row.append(str(self.xlBook.Worksheets(sheet_name).Cells(r, c)))
+            if row.count('None') >= col_num:  # 若该行全为None，则返回空行；反之只要有一个非None的值就正常返回row
+                row = []
+        return row
+
+    @cache
     def get_sheet(self, sheet_name):
-        A = []
-        for i in range(1, 10000):
-            check0=0
-            for j in range (1,10):
+        """读取Excel表中的一个sheet，返回表头各属性对应索引dict和数据表
+        注意：这里默认所有sheet都是矩阵，即所有行长度都等于表头长度"""
 
-                if str(self.xlBook.Worksheets(sheet_name).Cells(i, j)) == 'None' and i>10 :
-                    check0=check0+1
+        # 读取表头，属性-索引字典保存在header_dict中
+        r = 1
+        while len(self.get_a_row(sheet_name, r)) <= 0:
+            r += 1
+        header = self.get_a_row(sheet_name, r)  # 表头
+        r += 1
+        header_dict = {}
+        for i, name in enumerate(header):
+            header_dict[name] = i
 
-            if check0>=9:
-                break
-            check0=0
-            # print(self.xlBook.Worksheets(sheet_name).Rows(i))
-            B = []
-            for j in range(1, 100):
-                if (str(self.xlBook.Worksheets(sheet_name).Cells(i, j)) == 'None') and (
-                        str(self.xlBook.Worksheets(sheet_name).Cells(i, j + 1)) == 'None') and (
-                        str(self.xlBook.Worksheets(sheet_name).Cells(i, j + 2)) == 'None'):
-                            break
-                B.append(str(self.xlBook.Worksheets(sheet_name).Cells(i, j)))
-                print(str(self.xlBook.Worksheets(sheet_name).Cells(i, j)))
-            A.append(B)
-        # print(A)
-        return A
+        # 读取表中数据到data中
+        sheet_data = []
+        len_ = len(header_dict)  # 表头长度
+        row = self.get_a_row(sheet_name, r, len_)
+        while row:
+            sheet_data.append(row)
+            r += 1
+            row = self.get_a_row(sheet_name, r, len_)
+
+        return header_dict, sheet_data
 
     def close(self):
         self.xlBook.Close(self.filename)
@@ -47,23 +86,21 @@ class Easyexcel:
     def save(self):
         self.xlBook.Save()
 
-    def setSheet(self, sheet_name, content):
+    def set_sheet(self, sheet_name, content):
         sht = self.xlBook.Worksheets(sheet_name)
         for i in range(len(content)):
             for j in range(len(content[0])):
                 sht.Cells(i + 1, j + 1).Value = content[i][j]
         self.save()
 
-    def createSheet(self, sheet_name):
+    def create_sheet(self, sheet_name):
         sht = self.xlBook.Worksheets
         sht.Add(After='Sheet1').Name = sheet_name
         pass
-    def dataTableName(self):
-        dtn={"业务":0,"开票日期":1,"客户编号":2,"客户名称":3,"金额":4,"发票号码":5,"到期时间":6}
-        pass
 
 
-# 测试一下
-test = Easyexcel(r"C:\Project\RMB\昆山项目1\2018年04道普业务提成明细.xlsx","57578970","57578971")
-K = test.get_sheet('应收款4月份（数据源表）')
-print(K)
+if __name__ == '__main__':
+    excel = Easyexcel(os.getcwd() + r"\about\2018年04道普业务提成明细.xlsx", "57578970", "57578971")
+    header_dict, sheet_data = excel.get_sheet("应收款4月份（数据源表）")
+    print(len(header_dict))
+    print(len(sheet_data))
--- test.py
+++
@@ -1,5 +0,0 @@
-import xlrd as xlrd
-
-filename,password = "abc.xlsx", '123'
-xlwb = xlrd.open_workbook()
-print(xlwb.Sheets(1).Cells(1,1))
